d860566f97c62f823f2a83fc63dc003df3c59fd8
MG-yoonkh <yoonkh@mirineglobal.com>
2023-02-22 17:06:20 +0900

    20230210 課題

---
+++ Crawler2.py
@@ -0,0 +1,16 @@
+from pandas_datareader import data as pdr
+import yfinance as yf
+import matplotlib.pyplot as plt
+
+yf.pdr_override()
+
+samsung = pdr.get_data_yahoo('005930.KS', start='2020-07-01')
+print(samsung)
+
+print(samsung.index)
+print(samsung.columns)
+
+plt.plot(samsung.index, samsung.Close, 'b', label='Samsung Electronics')
+plt.show()
+
+samsung.to_csv("삼성전자.csv")
