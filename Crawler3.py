bf452094678826a598db67cb198df18d52dcb1e5
MG-yoonkh <yoonkh@mirineglobal.com>
2023-02-27 15:01:18 +0900

    20230227 課題

--- Crawler3.py
+++ Crawler3.py
@@ -2,24 +2,18 @@ import sys
 import traceback
 import pandas as pd
 import requests
+import matplotlib.pyplot as plt
 
-try:
-    code = sys.argv[1]
-    date = sys.argv[2]
-except Exception as e:
-    traceback.print_exc()
-
-# code = '005930'
+code = sys.argv[1]
+date = sys.argv[2]
 
 url = 'https://finance.naver.com/item/sise_day.naver?code={code}'.format(code=code)
 res = requests.get(url,headers={'User-Agent':'Mozilla/5.0'})
 res.encoding = 'euc-kr'
-res.status_code
+
 
 from bs4 import BeautifulSoup
 soap = BeautifulSoup(res.text, 'lxml')
-
-# print(soap)
 
 el_table_navi = soap.find("table", class_="Nnavi")
 el_td_last = el_table_navi.find("td", class_="pgRR")
@@ -40,18 +34,11 @@ def parse_page(code, page):
         traceback.print_exc()
     return None
 
-import datetime
-str_datefrom = datetime.datetime.strftime(datetime.datetime(year=2023, month=2, day=20), '%Y.%m.%d')
-str_datefrom
-
 df = None
-
-str_dateto = datetime.datetime.strftime(datetime.datetime.today(), '%Y.%m.%d')
-str_dateto
 
 for page in range(1, pg_last+1):
     _df = parse_page(code, page)
-    _df_filtered = _df[_df['날짜'] > str_datefrom]
+    _df_filtered = _df[_df['날짜'] > date]
     if df is None:
         df = _df_filtered
     else:
@@ -60,3 +47,17 @@ for page in range(1, pg_last+1):
         break
 
 print(df)
+
+
+fig = plt.figure()
+
+ax1 = fig.add_subplot(2,1,1)
+ax2 = fig.add_subplot(2,1,2)
+
+ax1.plot(df["날짜"],df["종가"], color="navy" , linewidth = 3)
+ax2.plot(df["날짜"],df["거래량"], color="green" , linewidth = 3)
+
+plt.show()
+
+
+df.to_csv('finance.csv', columns=['날짜', '종가', '시가', '거래량'])
