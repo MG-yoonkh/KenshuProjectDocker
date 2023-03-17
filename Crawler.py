3a637c849f8f9974651aa010a0841b6cddaa0cac
MG-yoonkh <yoonkh@mirineglobal.com>
2023-03-17 14:18:35 +0900

    first commit

--- Crawler.py
+++
@@ -1,9 +0,0 @@
-from urllib.request import urlopen
-from bs4 import BeautifulSoup
-
-html = urlopen("https://finance.naver.com/sise/sise_market_sum.naver?&page=")
-
-bsObject = BeautifulSoup(html, "html.parser")
-
-for link in bsObject.find_all('a'):
-    print(link.text.strip(), link.get_text('href'))
