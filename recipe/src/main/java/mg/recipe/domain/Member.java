7f9b50c77009dbcf38e23f5537e70b610cf34a23
MG-yoonkh <yoonkh@mirineglobal.com>
2023-03-20 15:47:28 +0900

    annotation commit

--- recipe/src/main/java/mg/recipe/domain/Member.java
+++ recipe/src/main/java/mg/recipe/domain/Member.java
@@ -1,47 +1,22 @@
 package mg.recipe.domain;
 
-import java.security.Timestamp;
-import java.time.LocalDateTime;
-
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter
-@Setter
-@Entity
 public class Member{
+    private Long id;
+    private String name;
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
+    public Long getId() {
+        return id;
+    }
 
-    @Column(length = 20)
-    private String userPassword;
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-    @Column(length = 40)
-    private String mailAddress;
+    public String getName() {
+        return name;
+    }
 
-    @Column(length = 20)
-    private String nickName;
-
-    @Column
-    private LocalDateTime birthDate;
-
-    @Column(length = 11)
-    private int phoneNum;
-
-    @Column(length = 200)
-    private String profileImage;
-
-    @Column(length = 1)
-    private boolean delFlag;
-
-    @Column
-    private Timestamp updateTime;
-
+    public void setName(String name) {
+        this.name = name;
+    }
 }
