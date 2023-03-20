c087a236c92cfec63482638213643929abd605f3
MG-parkhj <125540360+MG-parkhj@users.noreply.github.com>
2023-03-20 13:01:42 +0900

    adminPage

---
+++ recipe/frontend/js/index.js
@@ -0,0 +1,9 @@
+
+function service() {
+    let signIn = "${signIn}";
+    if (signIn == "") {
+        alert("ログインをお願いします。");
+        location.href = "${cpath}/login.html";
+        location.href = "$./login.html";
+    }
+}
