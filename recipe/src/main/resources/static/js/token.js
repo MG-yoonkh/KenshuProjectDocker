c0128d1e238a0b872873573648f6652d84a06beb
MG-parkhj <125540360+MG-parkhj@users.noreply.github.com>
2023-04-18 10:10:08 +0900

    script thymeleaf 追加

---
+++ recipe/src/main/resources/static/js/token.js
@@ -0,0 +1,4 @@
+  // Ajax 通信のため
+  var token = $("meta[name='_csrf']").attr("content");
+  var header = $("meta[name='_csrf_header']").attr("content");
+  $(document).ajaxSend(function (e, xhr, options) { xhr.setRequestHeader(header, token); });
