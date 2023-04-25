dd04159b6f23088faf30d662acb97ef6dbdf5bf6
MG-parkhj <125540360+MG-parkhj@users.noreply.github.com>
2023-04-25 10:27:20 +0900

    default img>random 修正

--- recipe/src/main/resources/static/js/loading.js
+++ recipe/src/main/resources/static/js/loading.js
@@ -6,7 +6,25 @@ document.onreadystatechange = function () {
 
 
 function setDefaultImage(img) {
-    img.src = '/assets/icon/cooking.png';
+    const randomNum = Math.floor(Math.random() * 5 + 1);
+    switch (randomNum){
+        case 1:
+          img.src = '/assets/icon/cooking.png';
+          break;
+        case 2:
+          img.src = '/assets/icon/cooking2.png';
+          break;
+        case 3:
+          img.src = '/assets/icon/salad.png';
+          break;
+        case 4:
+          img.src = '/assets/icon/egg.png';
+          break;
+        case 5:
+            img.src = '/assets/icon/fish.png';
+        break;
+
+  }
   }
 
   const expired = /*[[${expired}]]*/ false;
